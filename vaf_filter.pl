057f57af7c72efc4e907e055bd931f714325c778
scao <scao@genome.wustl.edu>
2017-08-21 17:25:46 -0500

    fix pindel filtering bug

--- vaf_filter.pl
+++ vaf_filter.pl
@@ -15,53 +15,131 @@ open(OUT2,">$f_vaf_out");
 
 foreach my $l (`cat $f_m`) 
 	{
-		my $ltr=$l; 
-		if($ltr=~/^#/) { next;  }
+		my $ltr=$l;
+		chomp($ltr);  
+		if($ltr=~/^#/) { print OUT1 $ltr,"\n"; next;  }
 		else {
-	 	my $info=$temp[7];
-		#my $r_info=$temp[8]; 
+		 my @temp=split("\t",$ltr); 
+	 	 my $info=$temp[7];
+		 my @temp2; 	
+		 my %rc; 
+		 my %rc2;
+		 my $r_tot;  
+		 my $r_tot2; 
+		 my $vaf_n;
+  		 my $vaf_t;
+		 my $ref;
+		 my $var;
+		 my $nt;
+		 my $ndp_ref;
+		 my $ndp_var;
+		 my $tdp_ref;
+		 my $tdp_var; 
 		 
-		if($info=~/strelka/) 
-		{
-		my $vaf_n=$temp[11];
-		my $vaf_t=$temp[12];
-		my $ref=$temp[3]; 
-		my $var=$temp[4];
-		my $r_tot=0; 	
-		my @temp2=split(":",$vaf_n);
-		my %rc=();			
+		 if($info=~/strelka/) 
+		 {
 		
-	 	 $rc{'A'}=split(",",$temp2[0])[0]; 
-		 $rc{'C'}=split(",",$temp2[1])[0];
-		 $rc{'G'}=split(",",$temp2[4])[0];
-			$rc{'T'}=split(",",$temp2[7])[0];
+			$vaf_n=$temp[11];
+		 	$vaf_t=$temp[12];
+		 	$ref=$temp[3]; 
+		 	$var=$temp[4];
+		 	$r_tot=0; 
+	
+		 	@temp2=split(":",$vaf_n);
+			%rc=();			
+			#my @temp3=split(",",$temp2[0]);
+			#$rc{'A'}=$temp3[0];
+	 	 	$rc{'A'}=(split(",",$temp2[0]))[0]; 
+		 	$rc{'C'}=(split(",",$temp2[1]))[0];
+		 	$rc{'G'}=(split(",",$temp2[4]))[0];
+		 	$rc{'T'}=(split(",",$temp2[7]))[0];
+			#print $vaf_n,"\n";
+			#print $vaf_t,"\n";	
+			#<STDIN>;
+			foreach my $nt (keys %rc) 
+			{
+				$r_tot+=$rc{$nt}; 
+				#print $rc{$nt},"\n";
+			}
 
-		foreach my $nt (keys %rc) 
+			@temp2=split(":",$vaf_t);
+
+        	%rc2=();
+
+         	$rc2{'A'}=(split(",",$temp2[0]))[0];
+         	$rc2{'C'}=(split(",",$temp2[1]))[0];
+         	$rc2{'G'}=(split(",",$temp2[4]))[0];
+         	$rc2{'T'}=(split(",",$temp2[7]))[0];
+
+        	foreach $nt (sort keys %rc2)
+        	{
+            	$r_tot2+=$rc2{$nt};
+				#print $rc2{$nt},"\n";
+        	}
+		#print $ltr,"\n";
+		my @vars=split(",",$var); 
+		my $rcvar=0;
+		my $rc2var=0; 
+		foreach my $v (@vars)
 		{
-			$r_tot+=$rc{$nt}; 
-		}
-
-		@temp2=split(":",$vaf_t);
-
-        my %rc2=();
-		my $r_tot2=0;
-
-         $rc2{'A'}=split(",",$temp2[0])[0];
-         $rc2{'C'}=split(",",$temp2[1])[0];
-         $rc2{'G'}=split(",",$temp2[4])[0];
-         $rc2{'T'}=split(",",$temp2[7])[0];
-
-        foreach my $nt (keys %rc)
-        {
-            $r_tot2+=$rc{$nt};
-        }
+		 $rcvar+=$rc{$v};
+		 $rc2var+=$rc2{$v}; 
+		}		
 		
-		print $temp[0],"\t",$temp[1],"\t",$temp[2],"\t",$temp[3],"\t",$temp[4],"\t",$rc{$ref},"\t",$rc{$ref}/$r_tot,"\t",$rc{$var},"\t",$rc{$var}/$r_tot,"\t",$rc2{$ref},"\t",$rc2{$ref}/$r_tot2,"\t",$rc2{$var},"\t",$rc2{$var}/$r_tot2,"\n"; 
+		#print $rc{$ref},"\t",$rcvar,"\t",$rc2{$ref},"\t",$rc2var,"\n"; 
+		#<STDIN>;
+		print OUT2 $temp[0],"\t",$temp[1],"\t",$temp[2],"\t",$temp[3],"\t",$temp[4],"\t",$info,"\t",$rc{$ref},"\t",$rc{$ref}/$r_tot,"\t",$rcvar,"\t",$rcvar/$r_tot,"\t",$rc2{$ref},"\t",$rc2{$ref}/$r_tot2,"\t",$rc2var,"\t",$rc2var/$r_tot2,"\n"; 
 
+		if($rc2var/$r_tot2>=0.05) {
+		        print OUT1 $ltr,"\n";} 	
 		}
 	
-		if($info=~/varscan/
+		elsif($info=~/varscan/ || $info=~/varindel/)
 		{
-			
+		   	$vaf_n=$temp[11];
+        	$vaf_t=$temp[12];
+			@temp2=split(":",$vaf_n); 
+			my @ndp4=split(",",$temp2[3]); 
+			$ndp_ref=$ndp4[0]+$ndp4[1];
+			$ndp_var=$ndp4[2]+$ndp4[3];
+            @temp2=split(":",$vaf_t);
+            my @tdp4=split(",",$temp2[3]);
+            $tdp_ref=$tdp4[0]+$tdp4[1];
+            $tdp_var=$tdp4[2]+$tdp4[3];
+			#print $ndp_ref,"\t",$ndp_var,"\t",$tdp_ref,"\t",$tdp_var,"\n";
+			#<STDIN>;
+	        print OUT2 $temp[0],"\t",$temp[1],"\t",$temp[2],"\t",$temp[3],"\t",$temp[4],"\t",$info,"\t",$ndp_ref,"\t",$ndp_ref/($ndp_ref+$ndp_var),"\t",$ndp_var,"\t",$ndp_var/($ndp_var+$ndp_ref),"\t",$tdp_ref,"\t",$tdp_ref/($tdp_ref+$tdp_var),"\t",$tdp_var,"\t",$tdp_var/($tdp_var+$tdp_ref),"\n";  
+		if($tdp_var/($tdp_var+$tdp_ref) >=0.05) 	
+		{
+		print OUT1 $ltr,"\n"; 
 		}
+		}
+
+		elsif($info=~/pindel/)
+        {
+
+			$vaf_t=$temp[9];
+            $vaf_n=$temp[10];
+			@temp2=split(":",$vaf_n);	
+			my @ndp2=split(",",$temp2[1]);
+            $ndp_ref=$ndp2[0];
+            $ndp_var=$ndp2[1];
+            @temp2=split(":",$vaf_t);
+            my @tdp2=split(",",$temp2[1]);
+            $tdp_ref=$tdp2[0];
+            $tdp_var=$tdp2[1];
+			#print $ndp_ref,"\t",$ndp_var,"\t",$tdp_ref,"\t",$tdp_var,"\n";
+			#<STDIN>;
+
+			print OUT2 $temp[0],"\t",$temp[1],"\t",$temp[2],"\t",$temp[3],"\t",$temp[4],"\t",$info,"\t",$ndp_ref,"\t",$ndp_ref/($ndp_ref+$ndp_var),"\t",$ndp_var,"\t",$ndp_var/($ndp_var+$ndp_ref),"\t",$tdp_ref,"\t",$tdp_ref/($tdp_ref+$tdp_var),"\t",$tdp_var,"\t",$tdp_var/($tdp_var+$tdp_ref),"\n";
+
+		if($tdp_var/($tdp_var+$tdp_ref)>=0.05) 
+		{
+		            print OUT1 $ltr,"\n";	
 	}
+		
+	}
+
+
+	}
+}
