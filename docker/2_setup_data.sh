acdf38ad6fe22cf8a7d6845164222a300e559e59
Matthew Wyczalkowski <m.wyczalkowski@wustl.edu>
2017-08-29 16:41:53 -0500

    Strelka completed successfully on test set
    * Changes here from host (outside of docker container)

--- docker/2_setup_data.sh
+++ docker/2_setup_data.sh
@@ -1,21 +1,30 @@
 # Set up data directory which will be used by SomaticWrapper
 # This will be a directory mounted within the container
+# Copy test data from Strelka for test purposes
+# to obtain the strelka data,
+#   cd /home/mwyczalk_test/src
+#   git clone https://github.com/Illumina/strelka
 
 # Note that we have to either copy BAM/BAI data, or create hard links to it.
-# Here we create hard links, but note that this requires that the DATA_DIR be in the same 
+# Here we copy. Note that hard links require that the DATA_DIR be in the same 
 # partition as the data
 
 DATA_DIR="/home/mwyczalk_test/data/docker/data/data"
+STRELKA_DIR="/home/mwyczalk_test/src/strelka"
 
 # Sample Name is used by Somatic Wrapper in the naming of the data files
 SAMPLE_NAME="SWtest"
 
 # define tumor, normal BAM and index files 
-N_BAM="/home/mwyczalk_test/src/SomaticWrapper/strelka/src/demo/data/NA12892_dupmark_chr20_region.bam"
-N_BAI="/home/mwyczalk_test/src/SomaticWrapper/strelka/src/demo/data/NA12892_dupmark_chr20_region.bam.bai"
+N_BAM="$STRELKA_DIR/src/demo/data/NA12892_demo20.bam"
+N_BAI="$STRELKA_DIR/src/demo/data/NA12892_demo20.bam.bai"
 
-T_BAM="/home/mwyczalk_test/src/SomaticWrapper/strelka/src/demo/data/NA12891_dupmark_chr20_region.bam"
-T_BAI="/home/mwyczalk_test/src/SomaticWrapper/strelka/src/demo/data/NA12891_dupmark_chr20_region.bam.bai"
+T_BAM="$STRELKA_DIR/src/demo/data/NA12891_demo20.bam"
+T_BAI="$STRELKA_DIR/src/demo/data/NA12891_demo20.bam.bai"
+
+# also copy reference
+REF="$STRELKA_DIR/src/demo/data/demo20.fa"
+REFI="$STRELKA_DIR/src/demo/data/demo20.fa.fai"
 
 # Create directory structure expected by Somatic Wrapper
 mkdir -p $DATA_DIR/$SAMPLE_NAME
@@ -34,5 +43,10 @@ cp $N_BAI ${SAMPLE_NAME}.N.bai
 cp $T_BAM ${SAMPLE_NAME}.T.bam
 cp $T_BAI ${SAMPLE_NAME}.T.bai
 
+# Reference data lives in a different place to keep from confusing SomaticWrapper
+mkdir -p $DATA_DIR/../A_Reference
+cp $REF $DATA_DIR/../A_Reference
+cp $REFI $DATA_DIR/../A_Reference
 
+echo Written test data to $DATA_DIR/$SAMPLE_NAME and $DATA_DIR/../A_Reference
 
