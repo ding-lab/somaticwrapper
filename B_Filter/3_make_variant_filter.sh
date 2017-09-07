8057a0814185709f5d796bed513784bd0e49028b
Matt Wyczalkowski <m.wyczalkowski@wustl.edu>
2017-09-07 03:20:37 +0000

    Compressing and indexing database

--- B_Filter/3_make_variant_filter.sh
+++ B_Filter/3_make_variant_filter.sh
@@ -10,14 +10,15 @@ JAR="/usr/local/snpEff/SnpSift.jar"
 DBSNP="$DATD/human_9606_b150_GRCh37p13.5col.vcf.gz"
 COSMICDB="$DATD/CosmicCodingMuts.grch37.v82.vcf.gz"
 
-# Output VCF
-OUT="$DATD/dbsnp.noCOSMIC.vcf"  # equivalant to pass.cosmic.vcf
 
-# if ANNO is defined, write out this intermediate file.  useful for debugging
+# if ANNO is defined, write out this intermediate file and don't compress final VCF.  useful for debugging
 # if ANNO is undefined (by commenting it out), will use pipeline vesion which does not generate an
-#   intermediate file.  
+#   intermediate file.  also compresses and indexes resulting file
 
 #ANNO="$DATD/dbsnp_cosmic_anno.vcf"
+
+# Output VCF.  Append '.gz' if ANNO is not defined
+OUT="$DATD/dbsnp.noCOSMIC.vcf.gz"  
 
 if [ ! -z "$ANNO" ]; then
 
@@ -31,8 +32,10 @@ echo Written to $OUT
 
 else
 
-java -jar $JAR annotate -id $COSMICDB $DBSNP  | java -jar $JAR filter -n " (exists ID) & (ID =~ 'COSM' ) "  > $OUT
-echo Written to $OUT
+java -jar $JAR annotate -id $COSMICDB $DBSNP  | java -jar $JAR filter -n " (exists ID) & (ID =~ 'COSM' ) " | bgzip > $OUT
+
+tabix -p vcf $OUT
+echo Written and indexed $OUT
 
 fi
 
