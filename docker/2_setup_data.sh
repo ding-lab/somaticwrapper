22caad9fa27f48fd8187cd9d183e5fb5a2a6f056
Matthew Wyczalkowski <m.wyczalkowski@wustl.edu>
2017-09-12 12:52:43 -0500

    Modifying demo BAM files to use standard chrom names

--- docker/2_setup_data.sh
+++ docker/2_setup_data.sh
@@ -1,5 +1,7 @@
 # Set up data directory which will be used by SomaticWrapper
-# This will be a directory mounted within the container
+# This script is run outside of the container
+# The output is a directory mounted within the container
+#
 # Copy test data from Strelka for test purposes
 # to obtain the strelka data,
 #   cd /home/mwyczalk_test/src
@@ -9,44 +11,57 @@
 # Here we copy. Note that hard links require that the DATA_DIR be in the same 
 # partition as the data
 
-DATA_DIR="/home/mwyczalk_test/data/docker/data/data"
+# To make VEP annotation work, we need to rename the chrom "demo20" to "20".  This
+# is done for both the BAM files and the reference
+
+DATA_DIR="/home/mwyczalk_test/data/docker/data/data"   # this ends up mapping to /data/data within container
+REF_DIR="/home/mwyczalk_test/data/docker/data/A_Reference"   # Reference data lives in a different place to keep from confusing SomaticWrapper
 STRELKA_DIR="/home/mwyczalk_test/src/strelka"
 
 # Sample Name is used by Somatic Wrapper in the naming of the data files
 SAMPLE_NAME="SWtest"
 
-# define tumor, normal BAM and index files 
+# Create directory structure expected by Somatic Wrapper
+SAMPLE_DIR="$DATA_DIR/$SAMPLE_NAME"
+mkdir -p $SAMPLE_DIR
+mkdir -p $REF_DIR
+
+# define original tumor, normal BAM and index files 
 N_BAM="$STRELKA_DIR/src/demo/data/NA12892_demo20.bam"
 N_BAI="$STRELKA_DIR/src/demo/data/NA12892_demo20.bam.bai"
 
 T_BAM="$STRELKA_DIR/src/demo/data/NA12891_demo20.bam"
 T_BAI="$STRELKA_DIR/src/demo/data/NA12891_demo20.bam.bai"
 
-# also copy reference
+# Reference data lives in a different place to keep from confusing SomaticWrapper
 REF="$STRELKA_DIR/src/demo/data/demo20.fa"
 REFI="$STRELKA_DIR/src/demo/data/demo20.fa.fai"
 
-# Create directory structure expected by Somatic Wrapper
-mkdir -p $DATA_DIR/$SAMPLE_NAME
 
-cd $DATA_DIR/$SAMPLE_NAME
+# Normally this is what we'd do:
+#cp $REF $REF_DIR
+#cp $REFI $REF_DIR
 
-# Make hard links to data
-#ln -i $N_BAM ${SAMPLE_NAME}.N.bam
-#ln -i $N_BAI ${SAMPLE_NAME}.N.bai
-#ln -i $T_BAM ${SAMPLE_NAME}.T.bam
-#ln -i $T_BAI ${SAMPLE_NAME}.T.bai
+#cp $N_BAM $SAMPLE_DIR/${SAMPLE_NAME}.N.bam
+#cp $N_BAI $SAMPLE_DIR/${SAMPLE_NAME}.N.bai
+#cp $T_BAM $SAMPLE_DIR/${SAMPLE_NAME}.T.bam
+#cp $T_BAI $SAMPLE_DIR/${SAMPLE_NAME}.T.bai
 
-# Copy data
-cp $N_BAM ${SAMPLE_NAME}.N.bam
-cp $N_BAI ${SAMPLE_NAME}.N.bai
-cp $T_BAM ${SAMPLE_NAME}.T.bam
-cp $T_BAI ${SAMPLE_NAME}.T.bai
+#####
+# so that this example works with vep annotation (which assumes real chrom names), we need to rename chrom "demo20" to standard name "20"
+# fix the reference first
+sed 's/demo20/20/g' $REF > $REF_DIR/demo20.fa
+samtools faidx $REF_DIR/demo20.fa
 
-# Reference data lives in a different place to keep from confusing SomaticWrapper
-mkdir -p $DATA_DIR/../A_Reference
-cp $REF $DATA_DIR/../A_Reference
-cp $REFI $DATA_DIR/../A_Reference
+echo writing to $SAMPLE_DIR
 
-echo Written test data to $DATA_DIR/$SAMPLE_NAME and $DATA_DIR/../A_Reference
+# Renaming 'demo20' -> '20' in BAM files and reindexing
+
+samtools view -h $N_BAM | sed 's/demo20/20/g' | samtools view -bT $REF_DIR/demo20.fa - -o $SAMPLE_DIR/${SAMPLE_NAME}.N.bam
+samtools index $SAMPLE_DIR/${SAMPLE_NAME}.N.bam $SAMPLE_DIR/${SAMPLE_NAME}.N.bai
+
+samtools view -h $T_BAM | sed 's/demo20/20/g' | samtools view -bT $REF_DIR/demo20.fa - -o $SAMPLE_DIR/${SAMPLE_NAME}.T.bam
+samtools index $SAMPLE_DIR/${SAMPLE_NAME}.T.bam $SAMPLE_DIR/${SAMPLE_NAME}.T.bai
+
+echo Written test data to $SAMPLE_DIR and $REF_DIR
 
