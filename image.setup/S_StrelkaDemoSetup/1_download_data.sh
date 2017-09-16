80f264549603c0d86730983879e4892d091690ea
Matthew A. Wyczalkowski <m.wyczalkowski@wustl.edu>
2017-09-16 01:02:09 +0000

    Download and install test Strelka data

---
+++ image.setup/S_StrelkaDemoSetup/1_download_data.sh
@@ -0,0 +1,21 @@
+
+#github repo: https://github.com/Illumina/strelka
+#path of interest: src/demo/data
+#
+#Downloading of individual folder described here:
+#https://stackoverflow.com/questions/7106012/download-a-single-folder-or-directory-from-a-github-repo
+
+# Requied
+# apt-get install subversion
+
+OUTD="/data/S_StrelkaTestData"
+mkdir -p $OUTD
+
+SRC="https://github.com/Illumina/strelka"
+DIR="src/demo/data"
+
+echo Downloading $DIR from $SRC
+
+svn checkout $SRC/trunk/$DIR/ $OUTD
+
+echo Written Strelka test data to $OUTD
--- image.setup/S_StrelkaDemoSetup/2_setup_strelka_demo.sh
+++ image.setup/S_StrelkaDemoSetup/2_setup_strelka_demo.sh
@@ -1,44 +1,46 @@
 # Set up data directory which will be used by SomaticWrapper
-# This script is run outside on the host
-# The output is a directory mounted within the container
 #
-# Copy test data from Strelka for test purposes
-# to obtain the strelka data,
-#   cd /home/mwyczalk_test/src
-#   git clone https://github.com/Illumina/strelka
-
-# Note that we have to either copy BAM/BAI data, or create hard links to it.
-# Here we copy. Note that hard links require that the DATA_DIR be in the same 
-# partition as the data
+# Using Strelka test data from https://github.com/Illumina/strelka/tree/master/src/demo/data
+# This is a compact dataset which has variants on chromosome "demo20"
+# Using NA12892_demo20.bam as Normal
+#       NA12891_demo20.bam as Tumor
 
 # To make VEP annotation work, we need to rename the chrom "demo20" to "20".  This
-# is done for both the BAM files and the reference
+# is done for both the BAM files and the reference. 
 
-DATA_DIR="/home/mwyczalk_test/data/docker/data/data"   # this ends up mapping to /data/data within container
-REF_DIR="/home/mwyczalk_test/data/docker/data/A_Reference"   # Reference data lives in a different place to keep from confusing SomaticWrapper
+# SomaticWrapper directory structure
+# /data/A_Reference - reference data
+# /data/A_Reference/demo20.fa - test reference
+# /data/data/SWtest - corresponds to individual sample SWtest for analysis (SAMPLE_DIR) 
+# /data/data/SWtest.N.bam, SWtest.T.bam - tumor and normal BAMs (may be links)
+
+DATA_BASE="/data"   
+DATD="$DATA_BASE/S_StrelkaTestData" # Where test data lives (BAM and reference)
+REF_DIR="$DATA_BASE/A_Reference"       # Reference data lives in a different place to keep from confusing SomaticWrapper
+
 STRELKA_DIR="/home/mwyczalk_test/src/strelka"
 
 # Sample Name is used by Somatic Wrapper in the naming of the data files
 SAMPLE_NAME="SWtest"
 
 # Create directory structure expected by Somatic Wrapper
-SAMPLE_DIR="$DATA_DIR/$SAMPLE_NAME"
+SAMPLE_DIR="$DATA_BASE/data/$SAMPLE_NAME"
 mkdir -p $SAMPLE_DIR
 mkdir -p $REF_DIR
 
 # define original tumor, normal BAM and index files 
-N_BAM="$STRELKA_DIR/src/demo/data/NA12892_demo20.bam"
-N_BAI="$STRELKA_DIR/src/demo/data/NA12892_demo20.bam.bai"
+N_BAM="$DATD/NA12892_demo20.bam"
+N_BAI="$DATD/NA12892_demo20.bam.bai"
 
-T_BAM="$STRELKA_DIR/src/demo/data/NA12891_demo20.bam"
-T_BAI="$STRELKA_DIR/src/demo/data/NA12891_demo20.bam.bai"
+T_BAM="$DATD/NA12891_demo20.bam"
+T_BAI="$DATD/NA12891_demo20.bam.bai"
 
 # Reference data lives in a different place to keep from confusing SomaticWrapper
-REF="$STRELKA_DIR/src/demo/data/demo20.fa"
-REFI="$STRELKA_DIR/src/demo/data/demo20.fa.fai"
+REF="$DATD/demo20.fa"
+REFI="$DATD/demo20.fa.fai"
 
 
-# Normally this is what we'd do:
+# If not remapping chrom, we would simply copy or make links to original data as,
 #cp $REF $REF_DIR
 #cp $REFI $REF_DIR
 
@@ -50,13 +52,12 @@ REFI="$STRELKA_DIR/src/demo/data/demo20.fa.fai"
 #####
 # so that this example works with vep annotation (which assumes real chrom names), we need to rename chrom "demo20" to standard name "20"
 # fix the reference first
-# Note that we still need to index this in A_Reference/3_index_strelka_demo.sh
 sed 's/demo20/20/g' $REF > $REF_DIR/demo20.fa
-
-echo writing to $SAMPLE_DIR
+# and index it
+bash ../A_Reference/prepare_reference.sh $REF_DIR/demo20.fa
 
 # Renaming 'demo20' -> '20' in BAM files and reindexing
-
+# 
 samtools view -h $N_BAM | sed 's/demo20/20/g' | samtools view -bT $REF_DIR/demo20.fa - -o $SAMPLE_DIR/${SAMPLE_NAME}.N.bam
 samtools index $SAMPLE_DIR/${SAMPLE_NAME}.N.bam $SAMPLE_DIR/${SAMPLE_NAME}.N.bai
 
