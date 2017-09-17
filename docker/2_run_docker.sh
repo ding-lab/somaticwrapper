cf417631c453365580074b150aaa18cb1a7f06e7
Matthew Wyczalkowski <m.wyczalkowski@wustl.edu>
2017-09-16 22:37:52 -0500

    Dockerfile fixes

--- docker/2_run_docker.sh
+++ docker/2_run_docker.sh
@@ -2,8 +2,8 @@
 # Note that DATA_DIR is mapped to the container
 # SomaticWrapper work directory is $DATA_DIR/data
 #   This allows directories not executed by SomaticWrapper (e.g., A_Reference) to exist on the data partition too
-DATA_DIR="/home/mwyczalk_test/src/SomaticWrapper/data"
-IMAGE="somatic-wrapper"
+DATA_DIR="$HOME/src/SomaticWrapper/data"
+IMAGE="mwyczalkowski/somatic-wrapper:latest"
 
 docker run -v $DATA_DIR:/data -it $IMAGE
 
