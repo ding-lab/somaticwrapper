ebb8afc5b2ecc5a49c00a10b8336a664d851812d
Matthew Wyczalkowski <mwyczalk@genome.wustl.edu>
2017-09-19 03:06:20 +0000

    Updated VEP and reference installation

---
+++ image.setup/A_Reference/3_get_GRCh38.sh
@@ -0,0 +1,20 @@
+# Download the GRCh38 human reference
+
+
+OUTD="/data/A_Reference"
+mkdir -p $OUTD
+
+echo Saving reference to $OUTD
+
+CWD=`pwd`
+cd $OUTD
+SRC="http://hgdownload.cse.ucsc.edu/goldenPath/hg38/bigZips/hg38.fa.gz"
+
+echo Downloading $SRC
+wget $SRC
+gunzip -v -f hg38.fa.gz
+
+cd $CWD
+
+echo Preparing reference GRCh38
+bash ./prepare_reference.sh $OUTD/hg38.fa
--- image.setup/D_VEP/1_install_cache.sh
+++ image.setup/D_VEP/1_install_cache.sh
@@ -6,5 +6,6 @@ cd /usr/local/ensembl-vep
 
 #perl INSTALL.pl -a cf -s homo_sapiens -y GRCh37,GRCh38 --CACHEDIR $CACHED
 perl INSTALL.pl -a cf -s homo_sapiens -y GRCh37 --CACHEDIR $CACHED
+perl INSTALL.pl -a cf -s homo_sapiens -y GRCh38 --CACHEDIR $CACHED
 
 cd $CWD
