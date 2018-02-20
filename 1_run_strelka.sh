bbe636acbd3e2bd06bf7592a96a3bf9fc361519c
Matthew Wyczalkowski <m.wyczalkowski@wustl.edu>
2018-02-20 16:32:32 -0600

    Removing per-step scripts
    * These are superfluous and possibly confusing.  Prefer to run SomaticWrapper.pl directly

--- 1_run_strelka.sh
+++
@@ -1,9 +0,0 @@
-# running Strelka
-# NOTE: to define configuration file, set on command line,
-#   export CONFIG="/gscuser/mwyczalk/projects/SomaticWrapper/config/01BR001.config"
-# before running this script.
-# This will overwrite the default configuration file below
-if [ -z $CONFIG ]; then
-    CONFIG=/data/data/SWtest/sw.config
-fi
-perl SomaticWrapper.pl 1 $CONFIG
