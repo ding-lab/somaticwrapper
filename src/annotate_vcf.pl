629a247fa7d90c1226a04f188972f13b5ccd44b7
Matthew A. Wyczalkowski <m.wyczalkowski@wustl.edu>
2018-04-01 02:21:13 +0000

    refining error exits

--- src/annotate_vcf.pl
+++ src/annotate_vcf.pl
@@ -84,6 +84,13 @@ export JAVA_OPTS=\"-Xms256m -Xmx512m\"
 
 $perl $gvip_dir/vep_annotator.pl $config_fn
 
+# Evaluate return value see https://stackoverflow.com/questions/90418/exit-shell-script-based-on-process-exit-code
+rc=\$? 
+if [[ \$rc != 0 ]]; then 
+    >&2 echo Fatal error \$rc: \$!.  Exiting.
+    exit \$rc; 
+fi
+
 EOF
 
     close OUT;
