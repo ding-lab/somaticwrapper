f94bc9d88bf5393c830b3e2a2465ce0d54a0d642
Matthew A. Wyczalkowski <m.wyczalkowski@wustl.edu>
2018-08-10 04:27:08 +0000

    Updates to test scripts - parse_pindel

--- testing/docker.testing/test.pindel_s7.sh
+++ testing/docker.testing/test.pindel_s7.sh
@@ -1,42 +1,26 @@
 # This is run from within a container typically started with ./start_docker.sh
 
-DATAD="../StrelkaDemo.dat"
-TUMOR_BAM=$DATAD/StrelkaDemoCase.T.bam
-NORMAL_BAM=$DATAD/StrelkaDemoCase.N.bam
-REFERENCE_FASTA=$DATAD/demo20.fa
-STRELKA_CONFIG=$DATAD/strelka.WES.ini
-VARSCAN_CONFIG=$DATAD/varscan.WES.ini
-DBSNP_DB=$DATAD/dbsnp-StrelkaDemo.noCOSMIC.vcf.gz
-CENTROMERE_BED=$DATAD/ucsc-centromere.GRCh37.bed
-#/home/mwyczalk_test/data/docker/data/B_Filter/dbsnp.noCOSMIC.GRCh37.vcf.gz
-#/home/mwyczalk_test/data/docker/data
-#VEP_CACHE_DIR=/home/mwyczalk_test/data/docker/data/D_VEP
-ASSEMBLY="GRCh37"
+REFERENCE_FASTA="/image/A_Reference/Homo_sapiens_assembly19.fasta"
+PINDEL_CONFIG="../StrelkaDemo.dat/pindel.WES.ini"
+DBSNP_DB="/image/B_Filter/dbsnp.noCOSMIC.GRCh37.vcf.gz"  # note that this differs from 00-All.brief.pass.cosmic.vcf.gz used in SW.  For present purposes that is OK
+PINDEL_RAW="/data/s5_run_pindel/results/pindel/pindel_out/pindel-raw.100-test.dat"
+#PINDEL_RAW="/data/s5_run_pindel/results/pindel/pindel_out/pindel-raw.dat"
 
 OUTDIR="./StrelkaDemo.results"
 mkdir -p $OUTDIR
 
-SAMPLE="fastq2bam_test"
-
-# Real path is /diskmnt/Projects/Users/hsun/beta_tinDaisy/compare/mgi_sw_C3N-01649/pindel/pindel.out.raw
-PINDEL_RAW="/data/pindel/pindel.out.raw"
+SAMPLE="C3N-01649"
 
 STEP=7
-
-if [ -z $STEP ]; then
-echo Must provide step number.  Usage:
-echo    bash run.SomaticWrapper.docker.sh STEP
-exit 1
-fi
 
 ARGS="\
 --reference_fasta $REFERENCE_FASTA \
 --pindel_config $PINDEL_CONFIG \
 --dbsnp_db $DBSNP_DB \
---no_delete_temp \
 --results_dir $OUTDIR \
 --pindel_raw $PINDEL_RAW \
 "
+#--no_delete_temp \
 
 # final output of step 10 is ./results/merged/merged.vcf (or ./results/vep/output.vcf.vep if --output_vep)
 
