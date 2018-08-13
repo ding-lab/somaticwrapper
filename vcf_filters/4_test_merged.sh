913e0a3347fd72c2071146c53392fc518e39cd57
Matthew A. Wyczalkowski <m.wyczalkowski@wustl.edu>
2018-08-13 16:07:05 +0000

    Testing merged filter

--- vcf_filters/4_test_merged.sh
+++ vcf_filters/4_test_merged.sh
@@ -3,14 +3,15 @@
 
 DATAD="/data"  # this works if running inside of docker
 
-MERGED_VCF="$DATAD/dat/td2.merged.short.vcf"
+VCF="$DATAD/dat/td2.merged.short.vcf"
+OUT="test.merged.vcf"
 
-export PYTHONPATH="somaticwrapper.cwl/vcf_filters:$PYTHONPATH"
-VAF_FILTER_LOCAL="merge_filter.py"  # filter module
+bash run_merged_filter.sh $VCF $OUT
 
-MAIN_FILTER="vcf_filter.py --no-filtered" # Assuming in path
-
-# Arguments to VAF filter
-SNV_VAF_ARGS="merged_caller --debug --include strelka-varscan" # --debug"
-$MAIN_FILTER --local-script $VAF_FILTER_LOCAL $MERGED_VCF $SNV_VAF_ARGS 
+#export PYTHONPATH="somaticwrapper.cwl/vcf_filters:$PYTHONPATH"
+#VAF_FILTER_LOCAL="merge_filter.py"  # filter module
+#MAIN_FILTER="vcf_filter.py --no-filtered" # Assuming in path
+## Arguments to VAF filter
+#SNV_VAF_ARGS="merged_caller --debug --include strelka-varscan" # --debug"
+#$MAIN_FILTER --local-script $VAF_FILTER_LOCAL $MERGED_VCF $SNV_VAF_ARGS 
 
