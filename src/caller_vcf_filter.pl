a75464de33e4c9dcfb3185aff5f0660b8e33f7c8
Matthew A. Wyczalkowski <m.wyczalkowski@wustl.edu>
2018-09-07 16:16:54 +0000

    Isolating caller VCF filter common to all parse_XXX workflows
    * calls VAF, indel length, and read depth
    * caller-specific fuctionality embedded in filter, passing caller argument to inform it
    * not tested

---
+++ src/caller_vcf_filter.pl
@@ -0,0 +1,55 @@
+# Perform post-call VCF filtering based on VAF, indel length, and read depth.
+# Parses varscan, pindel, and strelka VCFs
+# Filtering is performed by pyvcf-based scripts in ./vcf_filter
+# Configuration file defines all adjustable filter parameters.
+# Output filename is given by $output_vcf, written to $results_dir/caller_vcf_filter/$output_vcf
+
+sub caller_vcf_filter {
+    my $results_dir = shift;
+    my $job_files_dir = shift;
+    my $filter_dir = shift;
+    my $input_vcf = shift;  
+    my $output_vcf = shift;  #
+    my $caller = shift;  # strelka, varscan, or pindel
+    my $vcf_filter_config = shift;
+
+    die "Error: Input data file $input_vcf does not exist\n" if (! -e $input_vcf);
+    die "Error: Caller not defined\n" if (! $caller);
+
+    my $filter_results = "$results_dir/caller_vcf_filter";
+    system("mkdir -p $filter_results");
+
+# Create run script
+    my $runfn = "$job_files_dir/j_caller_vcf_filter.sh";
+    print STDERR "Writing to $runfn\n";
+    open(OUT, ">$runfn") or die $!;
+
+    my $vcf_filtered_fn = "$filter_results/$output_vcf";
+
+# Run vcf_filter.py family of filters: VAF, read depth, and indel length
+
+    print OUT <<"EOF";
+#!/bin/bash
+
+export JAVA_OPTS=\"-Xms256m -Xmx10g\"
+export VARSCAN_DIR="/usr/local"
+
+>&2 echo Running combined vcf_filter.py filters: VAF, read depth, and indel length
+export PYTHONPATH="$filter_dir:\$PYTHONPATH"
+bash $filter_dir/run_combined_vcf_filter.sh $dbsnp_filtered_fn $caller $vcf_filter_config $vcf_filtered_fn
+rc=\$?
+if [[ \$rc != 0 ]]; then
+    >&2 echo Fatal error \$rc: \$!.  Exiting.
+    exit \$rc;
+fi
+
+EOF
+    close OUT;
+    my $cmd = "bash < $runfn\n";
+    print STDERR "Executing:\n $cmd \n";
+
+    my $return_code = system ( $cmd );
+    die("Exiting ($return_code).\n") if $return_code != 0;
+}
+
+1;
