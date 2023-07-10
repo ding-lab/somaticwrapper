b92966c4c79a50275f33deb4f10bd6f447e94374
Cao <songcao@compute1-client-1.ris.wustl.edu>
2023-07-10 10:01:15 -0500

    update version label from 1.6.1 to v2.2

--- somaticwrapper.pl
+++ somaticwrapper.pl
@@ -1349,7 +1349,7 @@ sub bsub_qc_vcf{
     $current_job_file = "j9_qc_vcf.".$sample_name.".sh";
     my $IN_bam_T = $sample_full_path."/".$sample_name.".T.bam";
     my $IN_bam_N = $sample_full_path."/".$sample_name.".N.bam";
-``
+
     my $lsf_out=$lsf_file_dir."/".$current_job_file.".out";
     my $lsf_err=$lsf_file_dir."/".$current_job_file.".err";
     `rm $lsf_out`;
